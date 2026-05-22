a772af57f048bd72481f70437d34be404577c09f
gleepgorp <staphmaster2002@gmail.com>
2026-05-22 16:56:04 +0800

    [Misc][Vince] Register Aria gateway for deals

--- apps/api/src/deals/deals.module.ts
+++ apps/api/src/deals/deals.module.ts
@@ -3,9 +3,10 @@ import { DealsController } from './deals.controller'
 import { DealsService } from './deals.service'
 import { DealNotesService } from './deal-notes.service'
 import { AuditLogsModule } from '../audit-logs/audit-logs.module'
+import { AriaGatewayModule } from '../common/aria/aria-gateway.module'
 
 @Module({
-  imports: [AuditLogsModule],
+  imports: [AuditLogsModule, AriaGatewayModule],
   controllers: [DealsController],
   providers: [DealsService, DealNotesService],
   exports: [DealsService, DealNotesService],
