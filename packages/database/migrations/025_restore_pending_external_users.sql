faaef6f6fca4ac94821a74b2a11c4eac1e0bb006
gleepgorp <staphmaster2002@gmail.com>
2026-06-01 10:09:31 +0800

    [Fix][Vince] Restore pending external users

---
+++ packages/database/migrations/025_restore_pending_external_users.sql
@@ -0,0 +1,9 @@
+UPDATE users
+SET
+  deleted_at = NULL,
+  is_active = true,
+  "updatedAt" = NOW()
+WHERE status = 'pending'
+  AND role = 'PARTNER'
+  AND email IS NOT NULL
+  AND lower(email) NOT LIKE '%@symph.co';
